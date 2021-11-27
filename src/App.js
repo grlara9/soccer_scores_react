6c38555d7b5e4a594c4d3cee6838cfa659920470
Guillermo <grlara9@gmail.com>
2021-11-27 00:15:00 -0600

    i was able to make first call to api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import Soccer from './soccer/SoccerItem';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+//4c092769ed4424412311fbff39a27aa8	
+axios({
+  method: 'get',
+  url: 'https://v3.football.api-sports.io/leagues',
+  headers: {
+    'x-rapidapi-host': 'v3.football.api-sports.io',
+    'x-rapidapi-key': '4c092769ed4424412311fbff39a27aa8'
+}
+})
+.then(response =>{
+  console.log(response)
+})
+  
   return (
     <div className="App">
       <Soccer />
