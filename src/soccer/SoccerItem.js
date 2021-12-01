1996310ade1d3f05a5577dd5abf5089bfa13510f
Guillermo <grlara9@gmail.com>
2021-11-30 22:04:34 -0600

    started working on the skeleton  of my soccer container

--- src/soccer/SoccerItem.js
+++ src/soccer/SoccerItem.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Soccer = () =>{
     return(
-        <h1>hola</h1>
+        <div className="soocer">
+            <div className="soccer__title-box">
+                <p>Local</p>
+                <p>45</p>
+                <p>Visitor</p>
+            </div>
+
+            <div className="soccer__title-box">
+                <div className="team">
+                    <img className="homelogo" />
+                    <p>Team name</p>
+                </div>
+                <p className="goals"> 2 - 1 </p>
+                <div className="team">
+                    <img className="awaylogo" />
+                    <p>Team name</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
