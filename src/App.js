1558f2069ec210b15fe209e18667139375eceff2
Guillermo <grlara9@gmail.com>
2021-12-01 00:26:21 -0600

    fetch data for all live game

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 //4c092769ed4424412311fbff39a27aa8	
 axios({
   method: 'get',
-  url: 'https://v3.football.api-sports.io/leagues',
+  url: 'https://v3.football.api-sports.io/fixtures?live=all',
   headers: {
     'x-rapidapi-host': 'v3.football.api-sports.io',
     'x-rapidapi-key': '4c092769ed4424412311fbff39a27aa8'
