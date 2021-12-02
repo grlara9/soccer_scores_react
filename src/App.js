69978b7bbf54c4d638a53e769c3017d487bc79db
Guillermo <grlara9@gmail.com>
2021-12-02 00:35:46 -0600

    trying to get data but axios making alot of API calls

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
+import React, {useState, useEffect} from 'react'
 import Soccer from './soccer/SoccerItem';
 import './App.css';
 import axios from 'axios'
 
 function App() {
 
-//4c092769ed4424412311fbff39a27aa8	
-axios({
-  method: 'get',
-  url: 'https://v3.football.api-sports.io/fixtures?live=all',
-  headers: {
-    'x-rapidapi-host': 'v3.football.api-sports.io',
-    'x-rapidapi-key': '4c092769ed4424412311fbff39a27aa8'
-}
-})
-.then(response =>{
-  console.log(response)
-})
+  const [games, setGames] = useState([])
+  console.log("This is game >>>", games)
+
+useEffect(() => {
+  const getdata = async() =>{
+    const {data} = await axios({
+     
+       method: 'get',
+       url: 'https://v3.football.api-sports.io/fixtures?live=all',
+       headers: {
+         'x-rapidapi-host': 'v3.football.api-sports.io',
+         'x-rapidapi-key': '4c092769ed4424412311fbff39a27aa8',
+         
+     }
+     })
+     
+     setGames(data)
+    } 
+    getdata()
+  }, [])  
+
+  
+
   
   return (
     <div className="App">
